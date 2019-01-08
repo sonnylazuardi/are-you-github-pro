bca3eef2609f4781eaaadaf5bfb52a4607b90ac7
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2019-01-08 22:37:21 +0800

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const next = require('next');
-const fetch = require('isomorphic-fetch');
+const express = require("express");
+const bodyParser = require("body-parser");
+const next = require("next");
+const fetch = require("isomorphic-fetch");
+const cors = require("cors");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 
 const handle = app.getRequestHandler();
@@ -14,32 +15,33 @@ app
     const server = express();
 
     server.use(bodyParser.json());
+    server.use(cors());
 
-    server.post('/gdom', (req, res) => {
+    server.post("/gdom", (req, res) => {
       const query = req.body.query;
-      console.log('QUERY', query);
+      console.log("QUERY", query);
       fetch(`http://gdom.graphene-python.org/graphql`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json"
         },
-        body: JSON.stringify({ query }),
+        body: JSON.stringify({ query })
       })
         .then(res => res.json())
         .then(result => {
-          console.log('RESULT', result);
+          console.log("RESULT", result);
           res.json(result);
         });
     });
 
-    server.get('*', (req, res) => {
+    server.get("*", (req, res) => {
       return handle(req, res);
     });
 
     server.listen(3000, err => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+      console.log("> Ready on http://localhost:3000");
     });
   })
   .catch(ex => {
